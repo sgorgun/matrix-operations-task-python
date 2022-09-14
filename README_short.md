57f02ce7a21ee0a05833ad00725d4a7dd01c88d8
Dzmitry Dzemidka <ddzemidka@estee.com>
2022-09-14 15:21:13 +0300

    templates

---
+++ README_short.md
@@ -0,0 +1,15 @@
+# Matrix Operations
+
+Set of programming assignments that are designed to test knowledge of basic matrix operations.
+
+# Task 1. Matrix transposition 
+
+Your task is to implement a function for matrix transposition using a provided signature.
+
+# Task 2. Adding matrices 
+
+Your task is to implement a function for adding two matrices using a provided signature.
+
+# Task 3. Multiplying matrices 
+
+Your task is to implement a function for multiplying two matrices using a provided signature.
---
+++ tasks/matrix_operations.py
@@ -0,0 +1,47 @@
+"""Templates for programming assignments: basic matrix operations."""
+
+from typing import List
+
+
+def transpose(matrix: List[List[float]]) -> List[List[float]]:
+    """Returns a view of a given matrix with axes transposed.
+
+    Args:
+        matrix: List[List[float]], a given numeric matrix.
+
+    Returns:
+        The transposed matrix.
+    """
+    pass
+
+
+def add_matrices(matrix_a: List[List[float]], matrix_b: List[List[float]]) -> List[List[float]]:
+    """Returns a sum of two given matrices.
+
+    Args:
+        matrix_a: List[List[float]], the first numeric matrix.
+        matrix_b: List[List[float]], the second numeric matrix.
+
+    Returns:
+        The sum of two given matrices.
+
+    Raises:
+        ValueError: if given matrices are not compatible (dimensions differ in size).
+    """
+    pass
+
+
+def multiply_matrices(matrix_a: List[List[float]], matrix_b: List[List[float]]) -> List[List[float]]:
+    """Returns a product (result of multiplication) of two given matrices.
+
+    Args:
+        matrix_a: List[List[float]], the first numeric matrix.
+        matrix_b: List[List[float]], the second numeric matrix.
+
+    Returns:
+        The product of two given matrices.
+
+    Raises:
+        ValueError: if given matrices are not compatible.
+    """
+    pass
