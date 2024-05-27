215cbf2d60481611a1adcbc16dcb9e76652e5dac
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-05-27 15:42:26 +0300

    Done

--- tasks/matrix_operations.py
+++ tasks/matrix_operations.py
@@ -12,7 +12,14 @@ def transpose(matrix: List[List[float]]) -> List[List[float]]:
     Returns:
         The transposed matrix.
     """
-    pass
+    result = []
+    for col_index in range(len(matrix[0])):
+        row = []
+        for row_index in range(len(matrix)):
+            row.append(matrix[row_index][col_index])
+        result.append(row)
+
+    return result
 
 
 def add_matrices(matrix_a: List[List[float]], matrix_b: List[List[float]]) -> List[List[float]]:
@@ -28,7 +35,17 @@ def add_matrices(matrix_a: List[List[float]], matrix_b: List[List[float]]) -> Li
     Raises:
         ValueError: if the matrices are not compatible (dimensions differ in size).
     """
-    pass
+    if len(matrix_a) != len(matrix_b) or len(matrix_a[0]) != len(matrix_b[0]):
+        raise ValueError("Matrices are not compatible")
+
+    result = []
+    for row_index in range(len(matrix_a)):
+        row = []
+        for col_index in range(len(matrix_a[0])):
+            row.append(matrix_a[row_index][col_index] + matrix_b[row_index][col_index])
+        result.append(row)
+
+    return result
 
 
 def multiply_matrices(matrix_a: List[List[float]], matrix_b: List[List[float]]) -> List[List[float]]:
@@ -44,4 +61,17 @@ def multiply_matrices(matrix_a: List[List[float]], matrix_b: List[List[float]]) 
     Raises:
         ValueError: if the matrices are not compatible.
     """
-    pass
+    if len(matrix_a[0]) != len(matrix_b):
+        raise ValueError("Matrices are not compatible for multiplication.")
+    
+    result = []
+    for row_index in range(len(matrix_a)):
+        row = []
+        for col_index in range(len(matrix_b[0])):
+            element = 0
+            for k in range(len(matrix_a[0])):
+                element += matrix_a[row_index][k] * matrix_b[k][col_index]
+            row.append(element)
+        result.append(row)
+
+    return result
